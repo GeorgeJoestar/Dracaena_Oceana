61f647616f38c4f83882bd6445bfb954c8f487d2
jinkeloid <377844252@qq.com>
2025-03-11 00:52:35 +0800

    String standardization&Installer fix
    -字串注入方式对齐mod制作标准
    -性能鱼安装器(实际上就是个解压脚本)添加性能鱼本体

--- Fish_Installer.bat
+++ Fish_Installer.bat
@@ -3,4 +3,8 @@ powershell -NoProfile -ExecutionPolicy Bypass ^
   -Command "Expand-Archive -Path 'Fishery-main.zip' -DestinationPath 'temp_extract'"
 move temp_extract\Fishery-main ..\
 rmdir /s /q temp_extract
+powershell -NoProfile -ExecutionPolicy Bypass ^
+  -Command "Expand-Archive -Path 'Performance-Fish-main.zip' -DestinationPath 'temp_extract'"
+move temp_extract\Performance-Fish-main ..\
+rmdir /s /q temp_extract
 echo Done! The content has been moved to the parent folder.
